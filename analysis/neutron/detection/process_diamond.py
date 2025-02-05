8877e7c34ff780b17e0aae983d50c65d12e44842
remdelaportemathurin <rdelaportemathurin@gmail.com>
2025-02-05 11:00:13 +0100

    removed dead code + offset times

--- analysis/neutron/detection/process_diamond.py
+++ analysis/neutron/detection/process_diamond.py
@@ -1,14 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from libra_toolbox.neutron_detection.diamond.process_data import DataProcessor
-from pathlib import Path
-
-
-def add_files_from_folder(folder, data_proc):
-    for filename in Path(folder).rglob("*.CSV"):
-        data_proc.add_file(
-            filename, time_column=2, energy_column=3, delimiter=";", skip_header=1
-        )
+from datetime import datetime
 
 
 def add_files_from_npy(filename, data_proc):
@@ -18,69 +11,55 @@ def add_files_from_npy(filename, data_proc):
 
 
 # day 1 data part 1
+start_day_1_part_1 = datetime.strptime("12/10: 10:53am", "%m/%d: %I:%M%p")
 data_day_1_part_1 = DataProcessor()
 add_files_from_npy(
     "binary_data_day_1_part_1.npy",
     data_day_1_part_1,
 )
-# add_files_from_folder(
-#     data_proc=data_day_1_part_1,
-#     folder="../../../data/neutron_detection/Single diamond/20241210_LIBRA/UNFILTERED/",
-# )
-# np.save(
-#     f"binary_data_day_1_part_1.npy",
-#     np.array([data_day_1_part_1.time_values, data_day_1_part_1.energy_values]),
-# )
 
 # day 1 data part 2
+start_day_1_part_2 = datetime.strptime("12/10: 7:52pm", "%m/%d: %I:%M%p")
+
+# convert start time to seconds after start of part 1
+start_day_1_part_2 = (start_day_1_part_2 - start_day_1_part_1).total_seconds()
 data_proc_day_1_part2 = DataProcessor()
 add_files_from_npy(
     "binary_data_day_1_part_2.npy",
     data_proc_day_1_part2,
 )
-
-# add_files_from_folder(
-#     data_proc=data_proc_day_1_part2,
-#     folder="../../../data/neutron_detection/Single diamond/20241210_LIBRA_part2/UNFILTERED/",
-# )
-# np.save(
-#     f"binary_data_day_1_part_2.npy",
-#     np.array([data_proc_day_1_part2.time_values, data_proc_day_1_part2.energy_values]),
-# )
+data_proc_day_1_part2.time_values += start_day_1_part_2
 
 # day 2 data
+start_day_2 = datetime.strptime("12/11: 10:19am", "%m/%d: %I:%M%p")
+start_day_2 = (start_day_2 - start_day_1_part_1).total_seconds()
 data_proc_day_2 = DataProcessor()
 add_files_from_npy(
     "binary_data_day_2.npy",
     data_proc_day_2,
 )
-# add_files_from_folder(
-#     data_proc=data_proc_day_2,
-#     folder="../../../data/neutron_detection/Single diamond/20241211_LIBRA/DAQ/LIBRA_run2/UNFILTERED",
-# )
-# np.save(
-#     f"binary_data_day_2.npy",
-#     np.array([data_proc_day_2.time_values, data_proc_day_2.energy_values]),
-# )
+data_proc_day_2.time_values += start_day_2
+
 
 s_to_h = 1 / 3600
-
-fig, axs = plt.subplots(nrows=3, figsize=(10, 10), sharex=True)
 
 for i, data_proc in enumerate(
     [data_day_1_part_1, data_proc_day_1_part2, data_proc_day_2]
 ):
-    plt.sca(axs[i])
     rates, bins = data_proc.get_count_rate(bin_time=2)
     plt.plot(
         bins[:-1] * s_to_h,
         rates,
         label=data_proc,
     )
-    plt.gca().spines["top"].set_visible(False)
-    plt.gca().spines["right"].set_visible(False)
-    plt.ylabel("Counts per second")
-
+    plt.fill_between(
+        bins[:-1] * s_to_h,
+        rates,
+        alpha=0.3,
+    )
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+plt.ylabel("Counts per second")
 
 plt.xlabel("Time (h)")
 
