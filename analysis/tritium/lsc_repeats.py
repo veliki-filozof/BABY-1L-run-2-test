676d4771c005de7f925158aaaec0623194792670
Nikola Goles <golesn9@gmail.com>
2025-02-09 22:34:34 +0000

    Fixed and tested LSCRepeatSample functionality

--- analysis/tritium/lsc_repeats.py
+++ analysis/tritium/lsc_repeats.py
@@ -1,10 +1,20 @@
 import pint
-from typing import List, Dict
+from typing import List, Dict, DefaultDict
 import numpy as np
 import warnings
+from libra_toolbox.tritium.model import ureg, Model, quantity_to_activity
+import json
+from collections import defaultdict
+from libra_toolbox.tritium.lsc_measurements import (
+    LIBRARun,
+    LSCFileReader,
+    GasStream,
+    LSCSample,
+    LIBRASample,
+)
 
 def stdev_addition(stdevs: List[pint.Quantity]) -> pint.Quantity:
-    """Calculates stdev of added variables 
+    """Calculates stdev of summed variables 
     """
     result = np.sqrt(np.sum(std**2 for std in stdevs))
     return result
@@ -21,7 +31,7 @@ class LSCRepeatSample:
         self.name = name
         self.counted_activities = counted_activities
         self.rep_number = len(self.counted_activities)
-        self._perform_statistics(self)
+        self._perform_statistics()
         self.background_substracted = False
         self.origin_file = None
 
@@ -32,7 +42,7 @@ class LSCRepeatSample:
     def __str__(self):
         return f"Sample {self.name}"
     
-    def background_substract(self, background_sample: LSCRepeatSample):
+    def substract_background(self, background_sample: "LSCRepeatSample"):
         """Substracts the background activity from the sample activity, and updates standard deviation.
         Note: Does NOT substract background from individual counts, only from average
 
@@ -50,5 +60,32 @@ class LSCRepeatSample:
                 f"Activity of {self.name} is negative after substracting background. Setting to zero."
             )
             self.activity = 0 * ureg.Bq
-        self.stdev_activity = stdev_addition((self.stdev_activity, background_sample.stdev.activity))
+        self.stdev_activity = stdev_addition((self.stdev_activity, background_sample.stdev_activity))
         self.background_substracted = True
+
+    @staticmethod
+    def from_file(file_reader: LSCFileReader, vial_name: str) -> "LSCRepeatSample":
+        """Creates an LSCRepeatSample object from a LSCFileReader object
+
+        Args:
+            file_reader (LSCFileReader): LSCFileReader object
+            vial_name: Name of the vial
+
+        Raises:
+            ValueError: If vial_name is not found in the file reader
+
+        Returns:
+            the LSCSample object
+        """
+        values = file_reader.get_bq1_values()
+        labels = file_reader.vial_labels
+        if vial_name not in labels:
+            raise ValueError(f"Vial {vial_name} not found in the file reader.")
+        # Group values by labels
+        data: DefaultDict[str, List[float]] = defaultdict(list)
+        for label, value in zip(labels, values):
+            data[label].append(value)
+        activities = data[vial_name] * ureg.Bq
+        sample = LSCRepeatSample(vial_name, activities)
+        sample.origin_file = file_reader.file_path
+        return sample
