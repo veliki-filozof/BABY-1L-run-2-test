8046e36179aa46b69953a7d754d301e7dd10bf9e
Nikola Goles <golesn9@gmail.com>
2025-02-09 22:49:58 +0000

    Began adding higher level classes, will branch off to try a different approach

--- analysis/tritium/lsc_repeats.py
+++ analysis/tritium/lsc_repeats.py
@@ -1,8 +1,9 @@
 import pint
-from typing import List, Dict, DefaultDict
+from typing import List, Dict, DefaultDict, Literal
 import numpy as np
 import warnings
 from libra_toolbox.tritium.model import ureg, Model, quantity_to_activity
+from datetime import datetime, timedelta
 import json
 from collections import defaultdict
 from libra_toolbox.tritium.lsc_measurements import (
@@ -12,6 +13,9 @@ from libra_toolbox.tritium.lsc_measurements import (
     LSCSample,
     LIBRASample,
 )
+
+# config
+DATE_FORMAT = "%m/%d/%Y %I:%M %p"
 
 def stdev_addition(stdevs: List[pint.Quantity]) -> pint.Quantity:
     """Calculates stdev of summed variables 
@@ -35,6 +39,7 @@ class LSCRepeatSample:
         self.background_substracted = False
         self.origin_file = None
 
+    # Calculates mean and stdev of repeated counts
     def _perform_statistics(self):
         self.avg_activity = np.mean(self.counted_activities)
         self.stdev_activity = np.std(self.counted_activities, ddof=1) if self.rep_number > 1 else 0.0 
@@ -89,3 +94,121 @@ class LSCRepeatSample:
         sample = LSCRepeatSample(vial_name, activities)
         sample.origin_file = file_reader.file_path
         return sample
+    
+class LIBRARepeatSample:
+    samples = List[LSCRepeatSample]
+
+    def __init__(self, samples: List[LSCRepeatSample], time: str | datetime):
+        self.samples = samples
+
+        if isinstance(time, str):
+            self._time = datetime.strptime(time, DATE_FORMAT)
+        else:
+            self._time = time
+
+    def get_relative_time(self, start_time: str | datetime) -> timedelta:
+
+        if isinstance(start_time, str):
+            start_time = datetime.strptime(start_time, DATE_FORMAT)
+
+        sample_time = self._time
+        return sample_time - start_time
+
+    def substract_background(self, background_sample: LSCRepeatSample):
+        for sample in self.samples:
+            sample.substract_background(background_sample)
+
+    def get_soluble_activity(self):
+        act = 0
+        for sample in self.samples[:2]:
+            act += sample.activity
+
+        return act
+
+    def get_insoluble_activity(self):
+        act = 0
+        for sample in self.samples[2:]:
+            act += sample.activity
+
+        return act
+
+    def get_total_activity(self):
+        return self.get_soluble_activity() + self.get_insoluble_activity()
+    
+class RepeatGasStream:
+    samples: List[LIBRARepeatSample]
+    name: str | None
+
+    def __init__(
+        self,
+        samples: List[LIBRARepeatSample],
+        start_time: str | datetime,
+        name: str | None = None,
+    ):
+        """Creates a RepeatGasStream object from a list of LIBRARepeatSample objects
+
+        Args:
+            samples: a list of LIBRArepeatSample objects
+            start_time: the start time at which the gas stream was collected
+            name: a name for the RepeatGasStream. Defaults to None.
+        """
+        self.samples = samples
+        if isinstance(start_time, str):
+            self.start_time = datetime.strptime(start_time, DATE_FORMAT)
+        else:
+            self.start_time = start_time
+
+        self.name = name
+
+    def get_cumulative_activity(
+        self, form: Literal["total", "soluble", "insoluble"] = "total"
+    ):
+        """Calculates the cumulative activity of the gas stream
+
+        Args:
+            form: the form in which the cumulative activity is calculated.
+
+        Raises:
+            ValueError: If background has not been substracted
+
+        Returns:
+            the cumulative activity as a pint.Quantity object
+        """
+        # check that background has been substracted
+        if any(
+            not lsc_sample.background_substracted
+            for sample in self.samples
+            for lsc_sample in sample.samples
+        ):
+            raise ValueError(
+                "Background must be substracted before calculating cumulative activity"
+            )
+        cumulative_activity = []
+        for sample in self.samples:
+            if form == "total":
+                cumulative_activity.append(sample.get_total_activity())
+            elif form == "soluble":
+                cumulative_activity.append(sample.get_soluble_activity())
+            elif form == "insoluble":
+                cumulative_activity.append(sample.get_insoluble_activity())
+        cumulative_activity = ureg.Quantity.from_list(cumulative_activity)
+        cumulative_activity = cumulative_activity.cumsum()
+        return cumulative_activity
+
+    @property
+    def relative_times(self) -> List[timedelta]:
+        """
+        The relative times of the samples in the GasStream based on the start_time
+        as a list of timedelta objects
+        """
+        return [sample.get_relative_time(self.start_time) for sample in self.samples]
+
+    @property
+    def relative_times_as_pint(self) -> pint.facets.plain.PlainQuantity:
+        """
+        The relative times of the samples in the GasStream based on the start_time
+        as a pint.Quantity object
+        """
+        times = [t.total_seconds() * ureg.s for t in self.relative_times]
+        return ureg.Quantity.from_list(times).to(ureg.day)
+
