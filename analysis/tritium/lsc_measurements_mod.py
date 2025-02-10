7795bfbf122e2342f683cf4c5ae606d9db231c30
Nikola Goles <golesn9@gmail.com>
2025-02-10 23:48:38 +0000

    Added ability to handle standard deviations in GasGtreams

--- analysis/tritium/lsc_measurements_mod.py
+++ analysis/tritium/lsc_measurements_mod.py
@@ -170,7 +170,7 @@ class LSCSample:
                 f"Activity of {self.name} is negative after substracting background. Setting to zero."
             )
             self.activity = 0 * ureg.Bq
-        self.stdev = self.stdev_addition([self.stdev,background_sample.stdev])
+        self.stdev = self.stdev_addition([self.stdev, background_sample.stdev])
         self.background_substracted = True
 
     @staticmethod
@@ -298,6 +298,19 @@ class GasStream:
 
         self.name = name
 
+
+    @staticmethod
+    def append_stdev(stdev_list: List[pint.Quantity], stdev_val: pint.Quantity) -> List[pint.Quantity]:
+        """If the list is empty, appends value
+        Otherwise uses LSCSample.stdev_addition to calculate the stdev when new value is added, then appends
+        """
+        if not stdev_list:
+            stdev_list.append(stdev_val)
+        else:
+            stdev_list.append(LSCSample.stdev_addition([stdev_list[-1], stdev_val]))
+
+        return stdev_list
+
     def get_cumulative_activity(
         self, form: Literal["total", "soluble", "insoluble"] = "total"
     ):
@@ -310,8 +323,17 @@ class GasStream:
             ValueError: If background has not been substracted
 
         Returns:
-            the cumulative activity as a pint.Quantity object
+            if counts repeated:
+                tuple containing the cumulative activity and standard deviations as pint.Quantity object
+            else:
+                cumulative activity as a pint.Quantity
         """
+        # check if counts are repeated
+        is_repeated = any(
+            lsc_sample.repeated
+            for sample in self.samples
+            for lsc_sample in sample.samples
+        )
         # check that background has been substracted
         if any(
             not lsc_sample.background_substracted
@@ -322,16 +344,32 @@ class GasStream:
                 "Background must be substracted before calculating cumulative activity"
             )
         cumulative_activity = []
-        for sample in self.samples:
-            if form == "total":
-                cumulative_activity.append(sample.get_total_activity())
-            elif form == "soluble":
-                cumulative_activity.append(sample.get_soluble_activity())
-            elif form == "insoluble":
-                cumulative_activity.append(sample.get_insoluble_activity())
+        stdevs = []
+        if is_repeated:
+            for sample in self.samples:
+                if form == "total":
+                    act, stdev = sample.get_total_activity()
+                    cumulative_activity.append(act)
+                    self.append_stdev(stdevs, stdev)
+                elif form == "soluble":
+                    act, stdev = sample.get_soluble_activity()
+                    cumulative_activity.append(act)
+                    self.append_stdev(stdevs, stdev)
+                elif form == "insoluble":
+                    act, stdev = sample.get_insoluble_activity()
+                    cumulative_activity.append(act)
+                    self.append_stdev(stdevs, stdev)
+        else:
+            for sample in self.samples:
+                if form == "total":
+                    cumulative_activity.append(sample.get_total_activity())
+                elif form == "soluble":
+                    cumulative_activity.append(sample.get_soluble_activity())
+                elif form == "insoluble":
+                    cumulative_activity.append(sample.get_insoluble_activity())
         cumulative_activity = ureg.Quantity.from_list(cumulative_activity)
         cumulative_activity = cumulative_activity.cumsum()
-        return cumulative_activity
+        return (cumulative_activity, stdevs) if is_repeated else cumulative_activity
 
     @property
     def relative_times(self) -> List[timedelta]:
